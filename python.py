6705650ad6a67374138bf688ca931d5372380bb8
Pablo Loschi <loschi.pablo@gmail.com>
2019-05-07 17:17:23 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
- -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -16,8 +16,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-from __future__ import print_function
 
 import time
 from builtins import range
