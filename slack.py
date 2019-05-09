66d7b7522c3748cae374602275c10d42bb8fe405
Pablo Loschi <loschi.pablo@gmail.com>
2019-05-09 12:27:16 +0200

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,21 +1,9 @@
 from airflow.hooks.base_hook import BaseHook
-from airflow.operators.slack_operator import SlackAPIPostOperator
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 SLACK_CONN_ID = 'slack'
-
 def task_fail_slack_alert(context):
-    """
-    Sends message to a slack channel.
-    If you want to send it to a "user" -> use "@user",
-        if "public channel" -> use "#channel",
-        if "private channel" -> use "channel"
-    """
-    slack_channel = BaseHook.get_connection(SLACK_CONN_ID).login
-    slack_token = BaseHook.get_connection(SLACK_CONN_ID).password
-    failed_alert = SlackAPIPostOperator(
-        task_id='slack_failed',
-        channel=slack_channel,
-        token=slack_token,
-        text="""
+    slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    slack_msg = """
             :red_circle: Task Failed. 
             *Task*: {task}  
             *Dag*: {dag} 
@@ -28,5 +16,11 @@ def task_fail_slack_alert(context):
             exec_date=context.get('execution_date'),
             log_url=context.get('task_instance').log_url,
         )
-    )
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id='slack',
+        webhook_token=slack_webhook_token,
+        message=slack_msg,
+        username='airflow',
+        dag=dag)
     return failed_alert.execute(context=context)
